30782d294ec88d0ff4afc17715619f19ef7a88cd
Björn Mayer <bjoern.mayer@trivago.com>
2020-04-30 17:04:26 +0200

    change toInitialism into a parameter of abbr

--- src/main/kotlin/react/bootstrap/content/typography/Abbreviations.kt
+++ src/main/kotlin/react/bootstrap/content/typography/Abbreviations.kt
@@ -1,9 +1,12 @@
 package react.bootstrap.content.typography
 
 import kotlinx.html.ABBR
-import react.bootstrap.addClass
+import react.RBuilder
+import react.bootstrap.appendClass
 import react.bootstrap.lib.ClassNames
 import react.dom.RDOMBuilder
+import react.dom.abbr
 
 @Suppress("unused")
-fun RDOMBuilder<ABBR>.toInitialism() = addClass("${ClassNames.INITIALISM}")
+fun RBuilder.abbr(@Suppress("UNUSED_PARAMETER") initialism: Boolean, classes: String? = null, block: RDOMBuilder<ABBR>.() -> Unit) =
+    abbr(classes = classes.appendClass(ClassNames.INITIALISM), block = block)
