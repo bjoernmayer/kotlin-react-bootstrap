45d3fdc3c1ddbe5a0527828ec6f5c543805f833e
Björn Mayer <bjoern.mayer@trivago.com>
2020-05-02 22:37:44 +0200

    fix overflow in code

--- site/src/main/kotlin/react/bootstrap/site/components/Home.kt
+++ site/src/main/kotlin/react/bootstrap/site/components/Home.kt
@@ -1,8 +1,10 @@
 package react.bootstrap.site.components
 
+import react.RBuilder
 import react.RProps
 import react.bootstrap.content.img
 import react.bootstrap.content.typography.lead
+import react.bootstrap.content.typography.muted
 import react.bootstrap.layout.container
 import react.bootstrap.layout.grid.ItemsXs
 import react.bootstrap.layout.grid.Orderings
@@ -13,6 +15,7 @@ import react.bootstrap.lib.ClassNames
 import react.dom.br
 import react.dom.div
 import react.dom.h1
+import react.dom.p
 import react.functionalComponent
 import react.router.dom.routeLink
 
@@ -29,17 +32,17 @@ val Home = functionalComponent<RProps> {
                 }
                 col(md = Sizes.SZ_6) {
                     h1(classes = "${ClassNames.MB_3}") { +"Kotlin React Bootstrap" }
-                    lead {
+                    lead("${ClassNames.MB_4}") {
                         +"The most popular front-end framework"; br { }
                         +"Rebuilt for Kotlin React"
                     }
-                    row(classes = "${ClassNames.MB_N2}") {
-                        col(md = Sizes.AUTO, classes = "${ClassNames.PX_2}") {
+                    row(classes = "${ClassNames.MX_N2}") {
+                        col(lg = Sizes.EQ, classes = "${ClassNames.PX_2}") {
                             routeLink(PATH_DOCS, className = "btn btn-lg btn-bd-primary rounded-pill w-100 mb-3") {
                                 +"Get started"
                             }
                         }
-                        col(md = Sizes.AUTO, classes = "${ClassNames.PX_2}") {
+                        col(lg = Sizes.EQ, classes = "${ClassNames.PX_2}") {
                             routeLink(
                                 PATH_DOCS,
                                 className = "btn btn-lg btn-outline-secondary rounded-pill w-100 mb-3"
@@ -47,6 +50,9 @@ val Home = functionalComponent<RProps> {
                                 +"Components"
                             }
                         }
+                    }
+                    muted(RBuilder::p, "${ClassNames.MB_0}") {
+                        +"Currently WIP"
                     }
                 }
             }
