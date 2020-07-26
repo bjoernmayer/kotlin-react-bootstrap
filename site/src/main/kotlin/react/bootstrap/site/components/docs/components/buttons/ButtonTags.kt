bdcc7f4dfdebd481bad229aafab08323f78f7de4
Björn Mayer <bjoern.mayer@trivago.com>
2020-07-26 23:36:06 +0200

    use new code builders

--- site/src/main/kotlin/react/bootstrap/site/components/docs/components/buttons/ButtonTags.kt
+++ site/src/main/kotlin/react/bootstrap/site/components/docs/components/buttons/ButtonTags.kt
@@ -4,11 +4,12 @@ import kotlinx.html.ButtonType
 import react.RBuilder
 import react.bootstrap.components.button.Button
 import react.bootstrap.components.button.Buttons
+import react.bootstrap.site.components.docs.buildNestedName
+import react.bootstrap.site.components.docs.fixings.FunStyle
+import react.bootstrap.site.components.docs.fixings.Quoted
 import react.bootstrap.site.components.docs.fixings.SectionComponent
 import react.bootstrap.site.components.docs.fixings.codeExample
-import react.bootstrap.site.components.docs.fixings.import
 import react.bootstrap.site.components.docs.fixings.liveExample
-import react.bootstrap.site.components.docs.fixings.ln
 import react.bootstrap.site.external.Markdown
 
 internal class ButtonTags : SectionComponent() {
@@ -20,8 +21,8 @@ internal class ButtonTags : SectionComponent() {
             //language=Markdown
             +"""
 This library's `button` component is designed to be used with the HTML `button` element. However, you can also use these
-classes on `a` or `input` elements (though some browsers may apply a slightly different rendering). __Keep in mind, that
-the `input` element cannot have child elements.__
+classes on `a` or `input` elements (though some browsers may apply a slightly different rendering).
+__Keep in mind, that the `input` element cannot have child elements.__
             """
         }
         liveExample {
@@ -35,16 +36,87 @@ the `input` element cannot have child elements.__
             +" "
             Buttons.solid.primary(value = "Reset", type = Button.Types.Input.Type.RESET) { }
             +" "
-            Buttons.solid.primary(value = "true", title="Checkbox", type = Button.Types.Input.Type.CHECKBOX) { }
+            Buttons.solid.primary(value = "true", title = "Checkbox", type = Button.Types.Input.Type.CHECKBOX) { }
             +" "
-            Buttons.solid.primary(value = "true", title="Radio", type = Button.Types.Input.Type.RADIO) { }
+            Buttons.solid.primary(value = "true", title = "Radio", type = Button.Types.Input.Type.RADIO) { }
         }
         codeExample {
-            import("button.Button")
-            import("button.Button.button")
+            importButtonsBuilder()
             ln { }
-
-           // todo add code example
+            ktFun(
+                solidPrimaryFun,
+                solidButtonBuilderParents,
+                style = FunStyle.INLINE_BLOCK,
+                args = mapOf(
+                    "href" to Quoted("#")
+                )
+            ) {
+                string("Link")
+            }
+            ln(" ")
+            ktFun(
+                solidPrimaryFun,
+                solidButtonBuilderParents,
+                style = FunStyle.INLINE_BLOCK,
+                args = mapOf(
+                    "buttonType" to buildNestedName(ButtonType.submit.name, ButtonType::class.simpleName!!)
+                )
+            ) {
+                string("Button")
+            }
+            ln(" ")
+            ktFun(
+                solidPrimaryFun,
+                solidButtonBuilderParents,
+                style = FunStyle.INLINE_BLOCK,
+                args = mapOf(
+                    "value" to Quoted("Input")
+                )
+            ) {
+                string("Input")
+            }
+            ln(" ")
+            ktFun(
+                solidPrimaryFun,
+                solidButtonBuilderParents,
+                style = FunStyle.INLINE_BLOCK,
+                args = mapOf(
+                    "value" to Quoted("Submit"),
+                    "type" to Button.Types.Input.Type.SUBMIT.ktN
+                )
+            )
+            ln(" ")
+            ktFun(
+                solidPrimaryFun,
+                solidButtonBuilderParents,
+                style = FunStyle.INLINE_BLOCK,
+                args = mapOf(
+                    "value" to Quoted("Reset"),
+                    "type" to Button.Types.Input.Type.RESET.ktN
+                )
+            )
+            ln(" ")
+            ktFun(
+                solidPrimaryFun,
+                solidButtonBuilderParents,
+                style = FunStyle.INLINE_BLOCK,
+                args = mapOf(
+                    "value" to Quoted("true"),
+                    "title" to Quoted("Checkbox"),
+                    "type" to Button.Types.Input.Type.CHECKBOX.ktN
+                )
+            )
+            ln(" ")
+            ktFun(
+                solidPrimaryFun,
+                solidButtonBuilderParents,
+                style = FunStyle.INLINE_BLOCK,
+                args = mapOf(
+                    "value" to Quoted("true"),
+                    "title" to Quoted("Radio"),
+                    "type" to Button.Types.Input.Type.RADIO.ktN
+                )
+            )
         }
     }
 }
