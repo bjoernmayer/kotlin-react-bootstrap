67c9b636dec77c0ee55d6a8351030d347547b930
Björn Mayer <bjoern.mayer@trivago.com>
2020-07-27 22:04:22 +0200

    use new code builders

--- site/src/main/kotlin/react/bootstrap/site/components/docs/content/tables/Examples.kt
+++ site/src/main/kotlin/react/bootstrap/site/components/docs/content/tables/Examples.kt
@@ -5,13 +5,10 @@ package react.bootstrap.site.components.docs.content.tables
 import kotlinx.html.ThScope
 import react.RBuilder
 import react.bootstrap.content.tables.table
+import react.bootstrap.site.components.docs.fixings.FunStyle
 import react.bootstrap.site.components.docs.fixings.SectionComponent
 import react.bootstrap.site.components.docs.fixings.codeExample
-import react.bootstrap.site.components.docs.fixings.import
-import react.bootstrap.site.components.docs.fixings.ktF
-import react.bootstrap.site.components.docs.fixings.ktIF
 import react.bootstrap.site.components.docs.fixings.liveExample
-import react.bootstrap.site.components.docs.fixings.ln
 import react.bootstrap.site.components.docs.kt
 import react.bootstrap.site.external.Markdown
 import react.dom.tbody
@@ -75,33 +72,61 @@ inherited in Bootstrap 4__, meaning any nested tables will be styled in the same
         codeExample {
             import("content.tables.table")
             ln { }
-            ktF(0, RBuilder::table) {
-                ktF(it, RBuilder::thead) {
-                    ktF(it, RBuilder::tr) {
-                        ktIF(it, RBuilder::th, ThScope.col.kt, "+\"#\"")
-                        ktIF(it, RBuilder::th, ThScope.col.kt, "+\"First\"")
-                        ktIF(it, RBuilder::th, ThScope.col.kt, "+\"Last\"")
-                        ktIF(it, RBuilder::th, ThScope.col.kt, "+\"Handle\"")
+            ktFun(RBuilder::table) {
+                ktFun(RBuilder::thead) {
+                    ktFun(RBuilder::tr) {
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.col.kt)
+                        ) { string("#") }
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.col.kt)
+                        ) { string("First") }
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.col.kt)
+                        ) { string("Last") }
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.col.kt)
+                        ) { string("Handle") }
                     }
                 }
-                ktF(it, RBuilder::tbody) {
-                    ktF(it, RBuilder::tr) {
-                        ktIF(it, RBuilder::th, ThScope.row.kt, "+\"1\"")
-                        ktIF(it, RBuilder::td, "+\"Mark\"")
-                        ktIF(it, RBuilder::td, "+\"Otto\"")
-                        ktIF(it, RBuilder::td, "+\"@mdo\"")
+                ktFun(RBuilder::tbody) {
+                    ktFun(RBuilder::tr) {
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.row.kt)
+                        ) { string("1") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Mark") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Otto") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("@mdo") }
                     }
-                    ktF(it, RBuilder::tr) {
-                        ktIF(it, RBuilder::th, ThScope.row.kt, "+\"2\"")
-                        ktIF(it, RBuilder::td, "+\"Jacob\"")
-                        ktIF(it, RBuilder::td, "+\"Thornton\"")
-                        ktIF(it, RBuilder::td, "+\"@fat\"")
+                    ktFun(RBuilder::tr) {
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.row.kt)
+                        ) { string("2") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Jacob") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Thornton") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("@fat") }
                     }
-                    ktF(it, RBuilder::tr) {
-                        ktIF(it, RBuilder::th, ThScope.row.kt, "+\"3\"")
-                        ktIF(it, RBuilder::td, "+\"Larry\"")
-                        ktIF(it, RBuilder::td, "+\"the Bird\"")
-                        ktIF(it, RBuilder::td, "+\"@twitter\"")
+                    ktFun(RBuilder::tr) {
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.row.kt)
+                        ) { string("3") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Larry") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("the Bird") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("@twitter") }
                     }
                 }
             }
@@ -147,33 +172,49 @@ You can also invert the colors - with light text on dark background - by setting
         codeExample {
             import("content.tables.table")
             ln { }
-            ktF(0, RBuilder::table, "dark" to true) {
-                ktF(it, RBuilder::thead) {
-                    ktF(it, RBuilder::tr) {
-                        ktIF(it, RBuilder::th, ThScope.col.kt, "+\"#\"")
-                        ktIF(it, RBuilder::th, ThScope.col.kt, "+\"First\"")
-                        ktIF(it, RBuilder::th, ThScope.col.kt, "+\"Last\"")
-                        ktIF(it, RBuilder::th, ThScope.col.kt, "+\"Handle\"")
+            ktFun(RBuilder::table, args = mapOf("dark" to true)) {
+                ktFun(RBuilder::thead) {
+                    ktFun(RBuilder::tr) {
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.col.kt)
+                        ) { string("#") }
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.col.kt)
+                        ) { string("First") }
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.col.kt)
+                        ) { string("Last") }
+                        ktFun(
+                            RBuilder::th,
+                            style = FunStyle.INLINE_BLOCK,
+                            args = mapOf(null to ThScope.col.kt)
+                        ) { string("Handle") }
                     }
                 }
-                ktF(it, RBuilder::tbody) {
-                    ktF(it, RBuilder::tr) {
-                        ktIF(it, RBuilder::th, ThScope.row.kt, "+\"1\"")
-                        ktIF(it, RBuilder::td, "+\"Mark\"")
-                        ktIF(it, RBuilder::td, "+\"Otto\"")
-                        ktIF(it, RBuilder::td, "+\"@mdo\"")
+                ktFun(RBuilder::tbody) {
+                    ktFun(RBuilder::tr) {
+                        ktFun(RBuilder::th, args = mapOf(null to ThScope.row.kt)) { string("1") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Mark") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Otto") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("@mdo") }
                     }
-                    ktF(it, RBuilder::tr) {
-                        ktIF(it, RBuilder::th, ThScope.row.kt, "+\"2\"")
-                        ktIF(it, RBuilder::td, "+\"Jacob\"")
-                        ktIF(it, RBuilder::td, "+\"Thornton\"")
-                        ktIF(it, RBuilder::td, "+\"@fat\"")
+                    ktFun(RBuilder::tr) {
+                        ktFun(RBuilder::th, args = mapOf(null to ThScope.row.kt)) { string("2") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Jacob") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Thornton") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("@fat") }
                     }
-                    ktF(it, RBuilder::tr) {
-                        ktIF(it, RBuilder::th, ThScope.row.kt, "+\"3\"")
-                        ktIF(it, RBuilder::td, "+\"Larry\"")
-                        ktIF(it, RBuilder::td, "+\"the Bird\"")
-                        ktIF(it, RBuilder::td, "+\"@twitter\"")
+                    ktFun(RBuilder::tr) {
+                        ktFun(RBuilder::th, args = mapOf(null to ThScope.row.kt)) { string("3") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("Larry") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("the Bird") }
+                        ktFun(RBuilder::td, style = FunStyle.INLINE_BLOCK) { string("@twitter") }
                     }
                 }
             }
