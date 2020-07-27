cf6bc64973ad4a4e07210106729fe3910d379564
Björn Mayer <bjoern.mayer@trivago.com>
2020-07-27 23:35:12 +0200

    use new code builders

--- site/src/main/kotlin/react/bootstrap/site/components/docs/content/typography/InlineTextElements.kt
+++ site/src/main/kotlin/react/bootstrap/site/components/docs/content/typography/InlineTextElements.kt
@@ -1,18 +1,20 @@
 package react.bootstrap.site.components.docs.content.typography
 
+import kotlinx.html.SMALL
 import react.RBuilder
+import react.ReactElement
 import react.bootstrap.content.typography.mark
 import react.bootstrap.content.typography.small
+import react.bootstrap.site.components.docs.fixings.FunStyle
 import react.bootstrap.site.components.docs.fixings.SectionComponent
 import react.bootstrap.site.components.docs.fixings.codeExample
-import react.bootstrap.site.components.docs.fixings.import
-import react.bootstrap.site.components.docs.fixings.ktB
-import react.bootstrap.site.components.docs.fixings.ktIF
 import react.bootstrap.site.components.docs.fixings.liveExample
-import react.bootstrap.site.components.docs.fixings.ln
 import react.bootstrap.site.external.Markdown
+import react.dom.RDOMBuilder
 import react.dom.p
+import react.dom.small
 import react.dom.span
+import kotlin.reflect.KFunction3
 
 internal class InlineTextElements : SectionComponent() {
     override val title: String = "Inline text elements"
@@ -38,11 +40,15 @@ unwanted semantic implications that the tags would bring.
             import("content.typography.mark")
             import("content.typography.small")
             ln { }
-            ktB(0, "p") {
-                ln(it) { +"+\"You can use the mark tag to \";  mark(RBuilder::span) { +\"highlight\" }; +\" text.\"" }
+            ktBlock("p") {
+                ln { +"+\"You can use the mark tag to \";  mark(RBuilder::span) { +\"highlight\" }; +\" text.\"" }
             }
-            ktB(0, "p") {
-                ktIF(it, "small", "RBuilder::span", "+\"This line of text is meant to be treated as fine print.\"")
+            val smallFun: KFunction3<RBuilder,  String?,  (RDOMBuilder<SMALL>.() -> Unit), ReactElement>
+                = RBuilder::small
+            ktBlock("p") {
+                ktFun(smallFun, style = FunStyle.INLINE, args = mapOf(null to "RBuilder::span")) {
+                    string("This line of text is meant to be treated as fine print.")
+                }
             }
         }
     }
