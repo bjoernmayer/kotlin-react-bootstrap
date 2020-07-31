422f9d1899d1f35ab29c22bc943ba1674afdbedc
Björn Mayer <bjoern.mayer@trivago.com>
2020-07-31 13:24:12 +0200

    cleanup

--- site/src/main/kotlin/react/bootstrap/site/components/docs/components/alerts/Helpers.kt
+++ site/src/main/kotlin/react/bootstrap/site/components/docs/components/alerts/Helpers.kt
@@ -7,21 +7,14 @@ import react.bootstrap.components.alert.Alerts
 import react.bootstrap.components.alert.closingElement
 import react.bootstrap.components.alert.heading
 import react.bootstrap.components.alert.link
-import react.bootstrap.components.button.Button
 import react.bootstrap.site.components.docs.buildNestedName
 import kotlin.reflect.KClass
-
-internal val KClass<out Alert.Variants>.nestedName: String
-    get() = buildNestedName(this, Alert::class)
 
 internal val KClass<out Alert.Props>.nestedName: String
     get() = buildNestedName(this, Alert::class)
 
 internal val KClass<out Alert.DismissibleProps>.nestedName: String
     get() = buildNestedName(this, Alert::class)
-
-internal val Alert.Variants.kt: String
-    get() = buildNestedName(name, Alert::class, Button.Variants::class)
 
 internal val alertPropsName = Alert.Props::class.nestedName
 internal val closingElementName = RElementBuilder<Alert.DismissibleProps>::closingElement.name
