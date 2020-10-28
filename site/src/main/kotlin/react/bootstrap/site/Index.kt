20e0f20873a7db30c9e76cd6ae961da2ea1bdd69
Björn Mayer <bjoern.mayer@trivago.com>
2020-10-28 21:41:06 +0100

    stop require css in js for now, since kotlins loader impl seems to be broken

--- site/src/main/kotlin/react/bootstrap/site/Index.kt
+++ site/src/main/kotlin/react/bootstrap/site/Index.kt
@@ -1,6 +1,5 @@
 package react.bootstrap.site
 
-import kotlinext.js.require
 import kotlinx.browser.document
 import react.bootstrap.site.components.app
 import react.dom.render
@@ -9,9 +8,6 @@ import react.dom.render
 external val favicon: dynamic
 
 fun main() {
-    require("docs.min.css")
-    require("style.css")
-
     val rootDiv = document.getElementById("root")
     render(rootDiv) {
         app()
